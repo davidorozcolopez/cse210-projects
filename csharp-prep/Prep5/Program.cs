f2ad91c7bd541aae8629871c28c2d5708a784a5a
davidorozco <davidorozco@byui.edu>
2024-01-29 22:37:08 -0700

    Built program with functions that return username and a number squared.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,17 +1,19 @@
 using System;
 using System.Reflection.Metadata.Ecma335;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // functions
+        // Notes from class lesson
+        // Functions
         // returnType FunctionName(ParamType paramName1, ParamType paramName2)
         // {
         //   function body
         // }
 
-        int Add2(int number) {
+        static int Add2(int number) { // To define a standalone function in C#, use the static keyword before the return type.
             return number + 2;
         }
 
@@ -19,14 +21,14 @@ class Program
         System.Console.WriteLine(more);
 
         // void
-        void PrintName(string name) {
+        static void PrintName(string name) {
             if (name.Length == 0) {
                 return;
             }
             System.Console.WriteLine($"Name is {name}");
             return;
         }
-        PrintName("Bob");
+        PrintName("David");
 
         // variable scope
         var y = 0;
@@ -35,7 +37,46 @@ class Program
             w = y + 4;
             y = w + 5;
         }
-        // y = w + 4; doesn't work because w was declared locally inside curly braces
+        // y = w + 4; doesn't work here because "w" was declared locally inside curly braces
+
+        // Assignment #5
+        System.Console.WriteLine("\nAssignment #5: Functions calling functions\n");
+
+        static void DisplayWelcome()
+        {
+            System.Console.Write("Welcome to the Program!\n");
+        }
+
+        static string PromptUserName()
+        {
+            System.Console.Write("Please enter your username: ");
+            string username = Console.ReadLine();
+            return username;
+        }
+        
+        static int PromptUserNumber()
+        {
+            System.Console.Write("Please enter your favorite number: ");
+            int favoriteNumber = int.Parse(Console.ReadLine());
+            return favoriteNumber;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int squareNum = number * number;
+            return squareNum;
+        }
+
+        static void DisplayResult(string name, int numberSquared)
+        {
+            System.Console.WriteLine($"{name}, the square of your number is {numberSquared}");
+        }
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber);
 
     }
 
