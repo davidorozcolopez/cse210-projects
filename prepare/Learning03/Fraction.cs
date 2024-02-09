13d75655710551dafc48694cbb88d5957f2a3387
davidorozco <davidorozco@byui.edu>
2024-02-09 00:48:59 -0700

    Finished test cases and Fraction methods

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,5 +1,58 @@
-class Fraction
+using System;
+public class Fraction
 {
-    private int top;
+    // Attributes
+    private int top;   // private attributes that can only be accessed internally in the class or externally through the internal methods of this class
     private int bottom;
+
+    // Constructors
+    public Fraction() // Constructor that receives no parameters but sets default values to initialize the number to 1/1
+    {
+        top = 1;          // set default value of top to be 1
+        bottom = 1;       // set default value of bottom to be 1
+    }
+
+    public Fraction(int topNum) // Constructor that has one parameter for the top and that initializes the denominator to 1
+    {
+        top = topNum;      // assigning parameter to the attribute
+        bottom = 1;        // set default value of bottom to be 1
+    }
+
+    public Fraction(int topNum, int bottomNum) // Constructor that has two parameters, one for the top and one for the bottom
+    {
+        top = topNum;       // assigning values to attributes of class Fraction by assigning the parameters to the attributes
+        bottom = bottomNum;
+    }
+
+    // Getters and Setters
+    public int GetTop()
+    {
+        return top;
+    }
+
+    public void SetTop(int numerator)
+    {
+        top = numerator;
+    }
+
+    public int GetBottom()
+    {
+        return bottom;
+    }
+
+    public void SetBottom(int denominator)
+    {
+        bottom = denominator;
+    }
+
+    // Methods
+    public string GetFractionString()
+    {
+        return $"{top}/{bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)top / (double)bottom;
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();                     // create a new instance of class Fraction and call constructor with no parameters to set default values 1/1
+        System.Console.WriteLine(fraction1.GetFractionString()); // call GetFractionString() method
+        System.Console.WriteLine(fraction1.GetDecimalValue());   // call GetDecimalValue() method
+
+        Fraction fraction2 = new Fraction(1);                    // create a new instance of Fraction and call constructor with one parameter
+        System.Console.WriteLine(fraction2.GetFractionString()); // call GetFractionString() method
+        System.Console.WriteLine(fraction2.GetDecimalValue());   // call GetDecimalValue() method
+
+        fraction2.SetTop(5);                                     // call Setter SetTop() to set top of fraction to be 5
+        System.Console.WriteLine(fraction2.GetFractionString()); // call GetFractionString() method
+        System.Console.WriteLine(fraction2.GetDecimalValue());   // call GetDecimalValue() method
+
+        Fraction fraction3 = new Fraction(3, 4);                 // create new instance of Fraction and call constructor with two parameters
+        System.Console.WriteLine(fraction3.GetFractionString()); // call GetFractionString() method
+        System.Console.WriteLine(fraction3.GetDecimalValue());   // call GetDecimalValue() method
+
+        Fraction fraction4 = new Fraction(1, 3);                 // create new instance of Fraction and call constructor with two parameters
+        System.Console.WriteLine(fraction4.GetFractionString()); // call GetFractionString() method
+        System.Console.WriteLine(fraction4.GetDecimalValue());   // call GetDecimalValue() method
+
+        Fraction fraction5 = new Fraction();                     // create new instance of Fraction and call constructor with no parameters
+        fraction5.SetTop(7);                                     // call Setter SetTop() to set top of fraction to be 7 (change value)
+        System.Console.WriteLine(fraction5.GetTop());            // call Getter GetTop() to get top of fraction (retrieve value)
+        fraction5.SetBottom(8);                                  // call Setter SetBottom() to set bottom of fraction to be 8 (change value)
+        System.Console.WriteLine(fraction5.GetBottom());         // call Getter GetBottom() to get bottom of fraction (retrieve value)
+        System.Console.WriteLine(fraction5.GetFractionString()); // call GetFractionString() method
+        System.Console.WriteLine(fraction5.GetDecimalValue());   // call GetDecimalValue() method
     }
+
 }
