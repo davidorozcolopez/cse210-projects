83df28baefe86e10fa023f21c5bc3f1d04292913
davidorozco <davidorozco@byui.edu>
2024-02-13 00:26:19 -0700

    Hard-coded verse in Program class.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,8 @@ public class Program
 {
     static void Main(string[] args)
     {
-        string verse = "Hello";
+        string verse = "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
+
         System.Console.WriteLine("Hi");
     }
 }
