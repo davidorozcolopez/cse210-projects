c0d0c54295d81153908c4a21738da785a92926e8
davidorozco <davidorozco@byui.edu>
2024-02-18 18:15:00 -0700

    Attempted to handle case when all words are hidden.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.Intrinsics.Arm;
+using Microsoft.VisualBasic;
 public class Program
 {
     static void Main(string[] args)
@@ -8,7 +9,6 @@ public class Program
         
         Reference reference = new Reference("John", 3, 16);
         Scripture scripture = new Scripture(reference, verse);
-        Word word = new Word
 
         bool finished = false;
         bool hidden = false;
@@ -19,21 +19,20 @@ public class Program
             System.Console.WriteLine("Press enter to continue, type 'quit' to exit.");
             var choice = System.Console.ReadLine();
 
-            if (scripture.word != hidden)
-            {
-                if (choice == "quit")
-                {
-                    finished = true;
-                }
-                else
-                {
-                    scripture.HideThreeWords();
-                }
-            }
-            else
+            if (choice == "quit")
             {
                 finished = true;
             }
+            else
+                scripture.HideThreeWords();
+            
+                foreach (var word in scripture.wordsList) // How do I access the words?
+                {
+                    if (hidden)
+                    {
+                        finished = true;
+                    }
+                }
         }
     }
 }
