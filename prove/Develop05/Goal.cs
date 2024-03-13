773695ea6d588e6260f1d2a7b9d0c12a4c96e1c3
davidorozco <davidorozco@byui.edu>
2024-03-13 01:12:54 -0600

    Started writing structure of classes.

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,49 @@
+
+public class Goal
+{
+    protected int points;
+    protected string name;
+    protected string description;
+
+    public Goal(int points, string name, string description)
+    {
+        this.points = points;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int GetPoints()
+    {
+
+    }
+
+    public string GetName()
+    {
+
+    }
+
+    public string GetDescription()
+    {
+
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+
+    public bool IsComplete()
+    {
+
+    }
+
+    public void GoalMenu()
+    {
+
+    }
+
+    public override PerformTask()
+    {
+
+    }
+}
